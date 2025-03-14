cebe10b4e5b121d2adb477a01713d46f656b5b3e
ron131977 <ronalddsouza888@gmail.com>
2025-03-14 21:27:00 +0530

    ok new player once again

--- app/documentary/[slug]/page.tsx
+++ app/documentary/[slug]/page.tsx
@@ -662,8 +662,8 @@ export default async function DocumentaryPage({ params }: { params: { slug: stri
 
       <div className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          <div className="lg:col-span-2">
-            {/* <VideoPlayer
+          {/* <div className="lg:col-span-2">
+            <VideoPlayer
               videoUrl={documentary.videoUrl}
               thumbnailUrl={documentary.thumbnailUrl}
               title={documentary.title}
